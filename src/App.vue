4a165f9219011b4ce074309b867cd322c99ab558
passion-dev37 <passion.dev37@outlook.com>
2018-06-18 12:18:00 -0300

    add app component

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app id="app" style="background: linear-gradient(to right, #d3fbad, #d3fbad, #b6eae1);">
+    <MainComponent msg="Vuetify Example"></MainComponent>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MainComponent from './components/MainComponent.vue';
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    MainComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
